7b649cd715cb339e9a15d2818c34966c82be573c
Zar1915 <s1914216@jamaluddin.edu.mv>
2025-01-24 06:34:31 +0500

    Natural and Whole numbers

---
+++ Natural number.py
@@ -0,0 +1,8 @@
+number = int(input("Enter number"))
+print("The number to checked is ", number)
+if number>0:
+    print("This is a neutral number")
+elif number<0:
+    print("This is a whole number")    
+else: 
+    print("This is a whole number")
