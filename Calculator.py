7fb09c5581334cba1d7bc56bfe49353f45620504
Zar1915 <s1914216@jamaluddin.edu.mv>
2025-01-25 14:45:14 +0500

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,5 @@
+ 4
+num1 = int(input("Enter number one"))
+num2 = int(input("Enter number two"))
+print("The sum of the two numbers is ", num1 + num2)
+print("The product of the two numbers is ", num1 * num2)
