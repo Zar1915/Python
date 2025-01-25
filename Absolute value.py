bad4538ebaaf1d3d4c76ae35ce5e1645280ce11b
Zar1915 <s1914216@jamaluddin.edu.mv>
2025-01-25 14:16:40 +0500

    absolute value

---
+++ Absolute value.py
@@ -0,0 +1,11 @@
+def absolute_value(num):
+
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+print("The absolute value of 89", absolute_value(89))  
+print("The absolute value of -189", absolute_value(-189)) 
+
+    
