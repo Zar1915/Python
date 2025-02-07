d07ed86629e474f1e920e6aa49635831edf5946d
Zar1915 <s1914216@jamaluddin.edu.mv>
2025-02-07 14:38:59 +0500

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,17 @@
+my_tuple = ()
+print(my_tuple)
+my_tuple = (1, 2, 3)
+print(my_tuple)
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)
+my_tuple = ("Mouse", [8, 2, 1], (3, 2, 1))
+print(my_tuple)
+my_tuple = ("p", "e", "r", "m", "i", "t")
+print(my_tuple[0])
+print(my_tuple[5])
+n_tuple = ("Mouse", [8, 2, 1], (3, 2, 1))
+
+print(n_tuple[1][1])
+print("Sliced:", my_tuple[1:4])
+for letter in (my_tuple):
+    print("Hello", letter)
