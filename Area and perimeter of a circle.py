a095f5ba31d85c67652eafdc200b005a75ecc34f
Zar1915 <s1914216@jamaluddin.edu.mv>
2025-02-08 10:26:24 +0500

    Area and perimeter of circle

--- Area and perimeter of a circle.py
+++ Area and perimeter of a circle.py
@@ -3,8 +3,16 @@ class circle():
         self.radius = r
         self.pi = pi
     def Circlearea(self):
-        return self.radius
-NewCircle = circle(12, 10) 
+        return self.pi * self.radius *self.radius
+NewCircle = circle(12, 3.14) 
 print("Dimension of circle pi:%d radius:%d"%(NewCircle.radius, NewCircle.pi))
 print("Area of circle", NewCircle.Circlearea())
-        
+ 
+def calculate_perimeter(self):
+    return 2 * NewCircle.pi * NewCircle.radius
+NewCircle = circle(12, 3.14)
+print("Dimension of circle pi:%d radius:%d"%(NewCircle.radius, NewCircle.pi))
+radius = 12
+perimeter = calculate_perimeter(radius)
+print(f"The perimeter (circumference) of the circle is: {perimeter}")
+
---
+++ Random fake profile generator.py
@@ -0,0 +1,5 @@
+class Name:
+    def __init__(self,num=1):
+        ...
+        num = Total No. of Name You Want To Print
+        default is 1
