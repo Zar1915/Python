c41d28c62b831b70881a786d0c616a1ff27ea491
Zar1915 <s1914216@jamaluddin.edu.mv>
2025-02-14 08:42:16 +0500

    Slam book

--- disarium number.py
+++ disarium number.py
@@ -1,3 +1,6 @@
 num = int(input("Enter a number: "))
 total = sum(int(num[i]) ** (i + 1) for i in range(len(str(num))))
-print(f"{num} is a Disarium number." if total == num else f"{num} is NOT a Disarium number.")
+print(f"{num} is a Disarium number." 
+if total == num 
+else
+ f"{num} is NOT a Disarium number.")
