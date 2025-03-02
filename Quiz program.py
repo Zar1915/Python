6545d63ab182d956d02f6da01624b9a2fc900dcf
Zar1915 <s1914216@jamaluddin.edu.mv>
2025-03-02 11:58:08 +0500

    Quiz program

---
+++ Quiz program.py
@@ -0,0 +1,12 @@
+print("Do you want to play a quiz!")
+print("Let's do it")
+q1 = str(input("How many planets are in the solor system. Your options are (8, 9, 7, 5)"))
+if q1 == '8':
+    print("Correct!")
+elif q1 == '9':
+    print("Sorry, but your answer is wrong")
+elif q1 == '7':
+    print("Sorry, but your answer is wrong")
+elif q1 == '5':
+    print("Sorry, but your answer is wrong")
+    
